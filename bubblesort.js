5f16da532cb29606cbfb395d1f510890fa23e00e
Yoshie Fujiwara <yoshie.fujiwara@gmail.com>
2020-02-19 14:04:33 -0600

    first pass

--- bubblesort.js
+++ bubblesort.js
@@ -1,3 +1,31 @@
-function bubbleSort(array) {
-  return []
+function bubbleSort(arr) {
+  let finalArr = [];
+  let swapCall = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < 5; j++) {
+      console.log('nested loop');
+    }
+
+    // if (arr[i] > arr[i + 1]) {
+    //   console.log('num1', arr[i], 'num2', arr[i + 1]);
+  }
+  if (swapCall === 0) {
+    console.log('no swap', arr);
+    return arr;
+  }
+  //swap(arr[i], arr[i + 1]);
+  // console.log(arr);
+
+  console.log('finalArr', finalArr);
+  return finalArr;
+}
+
+//if swap is never been called, means arr is already sorted.
+
+// this swaps elements and returns in
+function swap(num1, num2) {
+  let temp = num1;
+  (num1 = num2), (num2 = temp);
+  swapCall += 1;
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,5 +1,13 @@
-describe('Bubble Sort', function(){
-  it('handles an empty array', function(){
-    expect( bubbleSort([]) ).toEqual( [] );
+describe('Bubble Sort', function() {
+  xit('handles an empty array', function() {
+    expect(bubbleSort([])).toEqual([]);
   });
+});
+
+it('handles array with elements', function() {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  //bubbleSort(arr);
+
+  expect(bubbleSort(arr)).toEqual([1, 2, 3, 4, 5, 6, 8, 7]);
 });
